e2ad99131a72d663cad9ca1a9ab1d636e4f1bbbb
meeejyu <alwn04016@naver.com>
2022-10-01 11:57:34 +0900

    프록시 예제 프로젝트 생성

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,8 +2,13 @@ package hello.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+
+@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록된다
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의, app 하위만 컴포넌트 스캔 대상으로 지정해둠
 public class ProxyApplication {
 
 	public static void main(String[] args) {
---
+++ proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package hello.proxy.app.v2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping // 해당 어노테이션은 컴포넌트 스캔 대상이 되지 않음
+@ResponseBody
+public class OrderControllerV2 {
+    
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
